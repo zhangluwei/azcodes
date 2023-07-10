0858402db98e67042d8fe97705a2f7a77dbced41
luwei <z_lwei@qq.com>
2023-07-10 12:47:06 +0800

    no commit message

--- compile.bat
+++ compile.bat
@@ -1,3 +1,22 @@
  
+7z x azerothcore-wotlk.zip.001
+7z x dll.zip.001
+
+set path=%path%;C:\azcodes\mysql8\bin
+set path=%path%;C:\azcodes\opensslv3\bin
+set MYSQL_INCLUDE_DIR = C:/azcodes\mysql8/include
+set MYSQL_LIBRARY = C:/azcodes\mysql8/lib/libmysql.lib
+set OPENSSL_ROOT_DIR=C:\azcodes\opensslv3
+set OPENSSL_INCLUDE_DIR=C:\azcodes\opensslv3/include
+set OPENSSL_CONF=C:\azcodes\opensslv3\bin\openssl.cfg
+set BOOST_ROOT=C:\azcodes\boost_1_82_0
+
+
 set
+
+cd build
+cmake -B c:/azcodes/build c:/azcodes/azerothcore-wotlk
+dir
+msbuild ALL_BUILD.vcxproj  /p:Configuration=Release
+
  
